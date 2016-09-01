036b29e6e6debec5e653d4afe82ced1db2b306ec
Yin Zhang <yzhang@cs.utexas.edu>
2016-09-01 22:59:15 +0800

    simplify clearState() implementation

--- nn-conv1d/PlanarConvolution.lua
+++ nn-conv1d/PlanarConvolution.lua
@@ -75,21 +75,6 @@ function PlanarConvolution:accGradParameters(input, gradOutput, scale)
 end
 
 function PlanarConvolution:clearState()
-   -- don't call set because it might reset referenced tensors
-   local function clear(f)
-      if self[f] then
-         if torch.isTensor(self[f]) then
-            self[f] = self[f].new()
-         elseif type(self[f]) == 'table' then
-            self[f] = {}
-         else
-            self[f] = nil
-         end
-      end
-   end
-   clear('output')
-   clear('gradInput')
-   clear('_input')
-   clear('_gradOutput')
-   return self
+   nn.utils.clear(self, '_input', '_gradOutput')
+   return nn.Module.clearState(self)
 end
