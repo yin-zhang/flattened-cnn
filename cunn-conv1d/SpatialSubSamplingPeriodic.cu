3ac624ef248c3057423cab817e28f0ec4f40bde3
Yin Zhang <yzhang@cs.utexas.edu>
2016-10-21 10:30:54 +0800

    add SpatialSubSamplingPeriodic

---
+++ cunn-conv1d/SpatialSubSamplingPeriodic.cu
@@ -0,0 +1,212 @@
+
+#include <thrust/transform.h>
+#include <thrust/reduce.h>
+#include <thrust/transform_reduce.h>
+#include <thrust/functional.h>
+
+
+/*
+ * Description:
+ */
+
+__device__ int translate_idx(int ii, int d1, int d2, int d3, int dW, int dH, int iW, int iH)
+{
+  int x, y, z, w;
+  w = ii % d3;
+  ii = ii/d3;
+  z = ii % d2;
+  ii = ii/d2;
+  y = ii % d1;
+  ii = ii/d1;
+  x = ii;
+  if ((w % dW != iW) || (z % dH != iH)) {
+    return -1;
+  }
+  w = w/dW;
+  z = z/dH;
+  d2 = (d2-iH)/dH;
+  d3 = (d3-iW)/dW;
+  return (((x*d1+y)*d2)+z)*d3+w;
+}
+
+__device__ int translate_idx_inv(int ii, int d1, int d2, int d3, int dW, int dH, int iW, int iH)
+{
+  int x, y, z, w;
+  w = ii % d3;
+  ii = ii/d3;
+  z = ii % d2;
+  ii = ii/d2;
+  y = ii % d1;
+  ii = ii/d1;
+  x = ii;
+  w = w*dW+iW;
+  z = z*dH+iH;
+  d2 *= dH;
+  d3 *= dW;
+  return (((x*d1+y)*d2)+z)*d3+w;
+}
+
+__global__ void downscale(float *input, float *output, long no_elements,
+                        int dW, int dH, int iW, int iH, int d1, int d2, int d3)
+{
+  // output offset:
+  long ii = threadIdx.x + blockDim.x * blockIdx.x;
+  ii += threadIdx.y + blockDim.y * (blockDim.x * gridDim.x) * blockIdx.y;
+  if (ii >= no_elements) return;
+  int ipidx = translate_idx_inv(ii, d1, d2, d3, dW, dH, iW, iH);
+  output[ii]=input[ipidx];
+}
+
+static int cunnconv1d_SpatialSubSamplingPeriodic_updateOutput(lua_State *L)
+{
+  THCState *state = getCutorchState(L);
+  THCudaTensor *input = (THCudaTensor*)luaT_checkudata(L, 2, "torch.CudaTensor");
+
+  int dW = luaT_getfieldcheckint(L, 1, "dW");
+  int dH = luaT_getfieldcheckint(L, 1, "dH");
+  int iW = luaT_getfieldcheckint(L, 1, "iW");
+  int iH = luaT_getfieldcheckint(L, 1, "iH");
+  THCudaTensor *output = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "output", "torch.CudaTensor");
+  
+  THCudaTensor_zero(state, output);
+
+  input = THCudaTensor_newContiguous(state, input);
+  // This is for allocating output Tensor
+  long no_elements = 1;
+  int i;
+  for(i = 0; i < input->nDimension - 2; i++){
+    no_elements *= input->size[i];
+  }
+  no_elements *= (input->size[i++] - iH) / dH;
+  no_elements *= (input->size[i++] - iW) / dW;
+
+  int d1;
+  int d2;
+  int d3;
+
+  if (input->nDimension == 3) {
+    d1 = output->size[0];
+    d2 = output->size[1];
+    d3 = output->size[2];
+  } else {
+    d1 = output->size[1];
+    d2 = output->size[2];
+    d3 = output->size[3];
+  }
+
+  float *input_data = THCudaTensor_data(state, input);
+  float *output_data = THCudaTensor_data(state, output);
+
+  // cuda blocks & threads:
+  long nthreads = 256;
+  // Max number of blocks: http://en.wikipedia.org/wiki/CUDA
+  // 65535 for SM 2.x, 2^32 -1 for >= 3.0
+  // TODO: When we move to SM 3.5 we should update this
+  long n_xblocks = min(max((int)ceil((float)no_elements / nthreads), 1), 65535);
+  long n_yblocks = (long)ceil((float)no_elements / (float)(n_xblocks * nthreads));
+  if (n_yblocks > 65535) {
+    THError("Input size is too large!  aborting");
+  }
+  dim3 blocks(n_xblocks, n_yblocks);
+  dim3 threads(nthreads);
+
+  // kernel:
+  downscale<<<blocks, threads, 0, THCState_getCurrentStream(state)>>> (input_data, output_data, no_elements, dW, dH, iW, iH, d1, d2, d3);
+  THCudaCheck(cudaGetLastError());
+
+  // final cut:
+  THCudaTensor_free(state, input);
+
+  return 1;
+}
+
+/*
+ * Description:
+ */
+__global__ void upscale(float *gradInput_data, float *gradOutput_data, long no_elements,
+                              int dW, int dH, int iW, iH, int d1, int d2, int d3)
+{
+  // output offset:
+  long ii = threadIdx.x + blockDim.x * blockIdx.x;
+  ii += threadIdx.y + blockDim.y * (blockDim.x * gridDim.x) * blockIdx.y;
+  if (ii >= no_elements) return;
+  int ipidx = translate_idx(ii, d1, d2, d3, dW, dH, iW, iH);
+  if (ipidx >= 0) {
+    gradInput_data[ii] += gradOutput_data[ipidx];
+  }
+}
+
+
+static int cunnconv1d_SpatialSubSamplingPeriodic_updateGradInput(lua_State *L)
+{
+
+  THCState *state = getCutorchState(L);
+  THCudaTensor *input =  (THCudaTensor *)luaT_checkudata(L, 2, "torch.CudaTensor");
+  THCudaTensor *gradOutput = (THCudaTensor *)luaT_checkudata(L, 3, "torch.CudaTensor");
+
+  int dW = luaT_getfieldcheckint(L, 1, "dW");
+  int dH = luaT_getfieldcheckint(L, 1, "dH");
+  int iW = luaT_getfieldcheckint(L, 1, "iW");
+  int iH = luaT_getfieldcheckint(L, 1, "iH");
+  THCudaTensor *gradInput = (THCudaTensor *)luaT_getfieldcheckudata(L, 1, "gradInput", "torch.CudaTensor");
+
+  THCudaTensor_zero(state, gradInput);
+
+  float *gradInput_data = THCudaTensor_data(state, gradInput);
+  float *gradOutput_data = THCudaTensor_data(state, gradOutput);
+
+  long no_elements = 1;
+  for(int i = 0; i < gradInput->nDimension; i++){
+    no_elements *= gradInput->size[i];
+  }
+
+  int d1;
+  int d2;
+  int d3;
+
+  if (gradInput->nDimension == 3) {
+    d1 = gradInput->size[0];
+    d2 = gradInput->size[1];
+    d3 = gradInput->size[2];
+  } else {
+    d1 = gradInput->size[1];
+    d2 = gradInput->size[2];
+    d3 = gradInput->size[3];
+  }
+
+  // cuda blocks & threads:
+  long nthreads = 256;
+  // Max number of blocks: http://en.wikipedia.org/wiki/CUDA
+  // 65535 for SM 2.x, 2^32 -1 for >= 3.0
+  // TODO: When we move to SM 3.5 we should update this
+  long n_xblocks = min(max((int)ceil((float)no_elements / nthreads), 1), 65535);
+  long n_yblocks = (long)ceil((float)no_elements / (float)(n_xblocks * nthreads));
+  if (n_yblocks > 65535) {
+    THError("Input size is too large!  aborting");
+  }
+  dim3 blocks(n_xblocks, n_yblocks);
+  dim3 threads(nthreads);
+
+  // kernel:
+  upscale<<<blocks, threads, 0, THCState_getCurrentStream(state)>>> (gradInput_data, gradOutput_data, no_elements,
+    dW, dH, iW, iH, d1, d2, d3);
+  THCudaCheck(cudaGetLastError());
+
+  return 1;
+}
+
+
+static const struct luaL_Reg cunnconv1d_SpatialSubSamplingPeriodic__ [] = {
+   {"SpatialSubSamplingPeriodic_updateOutput", cunnconv1d_SpatialSubSamplingPeriodic_updateOutput},
+   {"SpatialSubSamplingPeriodic_updateGradInput", cunnconv1d_SpatialSubSamplingPeriodic_updateGradInput},
+   {NULL, NULL}
+};
+
+
+void cunnconv1d_SpatialSubSamplingPeriodic_init(lua_State *L)
+{
+   luaT_pushmetatable(L, "torch.CudaTensor");
+   luaT_registeratname(L, cunnconv1d_SpatialSubSamplingPeriodic__, "nn");
+   lua_pop(L,1);
+}
+
---
+++ nn-conv1d/SpatialSubSamplingPeriodic.lua
@@ -0,0 +1,73 @@
+local SpatialSubSamplingPeriodic, parent = torch.class('nn.SpatialSubSamplingPeriodic', 'nn.Module')
+
+--[[
+Applies a 2D up-sampling over an input image composed of several input planes.
+
+    The upsampling is done in a periodic fashion.  That is: only output coodinates that are multiple of
+    scale_factor has non-zero values.
+
+The Y and X dimensions are assumed to be the last 2 tensor dimensions.  For
+instance, if the tensor is 4D, then dim 3 is the y dimension and dim 4 is the x.
+
+owidth  = width*scale_factor
+oheight  = height*scale_factor
+--]]
+
+function SpatialSubSamplingPeriodic:__init(dW,dH,iW,iH)
+   parent.__init(self)
+
+   self.dW = dW
+   self.dH = dH
+   self.iW = iW
+   self.iH = iH
+   
+   if self.dW < 1 or self.dH < 1 then
+       error('scale_factor must be no less than 1')
+   end
+
+   if math.floor(dW) ~= dW or math.floor(dH) ~= dH or math.floor(iW) ~= iW or math.floor(iH) ~= iH then
+       error('dW, dH, iW, iH must be integer')
+   end
+
+   if iW < 0 or iW >= dW then
+       error('iW must satisfy 0 <= iW < dW')
+   end
+
+   if iH < 0 or iH >= dH then
+       error('iH must satisfy 0 <= iH < dH')
+   end
+   
+   self.inputSize = torch.LongStorage(4)
+   self.outputSize = torch.LongStorage(4)
+   self.usage = nil
+end
+
+function SpatialSubSamplingPeriodic:updateOutput(input)
+   if input:dim() ~= 4 and input:dim() ~= 3 then
+     error('SpatialSubSamplingPeriodic only support 3D or 4D tensors')
+   end
+   -- Copy the input size
+   local xdim = input:dim()
+   local ydim = input:dim() - 1
+   for i = 1, input:dim() do
+     self.inputSize[i] = input:size(i)
+     self.outputSize[i] = input:size(i)
+   end
+   self.outputSize[ydim] = math.ceil(self.outputSize[ydim] / self.dH)
+   self.outputSize[xdim] = math.ceil(self.outputSize[xdim] / self.dW)
+   -- Resize the output if needed
+   if input:dim() == 3 then
+     self.output:resize(self.outputSize[1], self.outputSize[2],
+       self.outputSize[3])
+   else
+     self.output:resize(self.outputSize)
+   end
+   input.nn.SpatialSubSamplingPeriodic_updateOutput(self, input)
+   return self.output
+end
+
+function SpatialSubSamplingPeriodic:updateGradInput(input, gradOutput)
+   self.gradInput:resizeAs(input)
+   input.nn.SpatialSubSamplingPeriodic_updateGradInput(self, input, gradOutput)
+   return self.gradInput
+end
